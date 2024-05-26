4cdded8a15ed221753e2c70e61446399aba0617b
KarunyaReddyJ <Karunyareddy1418@gmail.com>
2024-05-26 12:03:31 +0530

    Made the ChatRoom with style

--- public/script.js
+++ public/script.js
@@ -3,23 +3,14 @@ let userName=''
 const socket=io.connect(window.location.origin)
 const form=document.querySelector('#form')
 const messages=document.querySelector('#messages')
-if(!loggedIn){
-    document.querySelector('#join').addEventListener('click',()=>{
-        const username=document.querySelector('#username').value
-        if(username===""){
-            alert('Enter Name')
-        }
-        else{
-            userName=username
-            messages.style.display='block'
-            form.style.display='block'
-            document.querySelector('#user').style.display='none'
-            loggedIn=true
-            socket.emit('connection',{username})
-        }
-    })
+do{
+    userName=prompt('Enter your name: ')
+}while(userName.trim()==='')
+if(userName){
+    loggedIn=true
+    socket.emit('connection',{username:userName})
 }
-socket.on('connection',(name)=>{
+socket.on('connection',({name})=>{
     alert(name.username,' has joined')
 })
 document.getElementById('submit').addEventListener('click',(e)=>{
@@ -36,14 +27,35 @@ socket.on('chat',(msg)=>{
     const message=document.createElement('li')
     message.style.cssText='list-style-type:none;width:100%'
     
-    if(msg.username===userName){
-        message.innerText=`You: ${msg.message}`
-        message.style.cssText=`text-align:right;margin:5px 10px`
-    }
-    else{
-        message.innerText=`${msg.username}:${msg.message}`
-        message.style.cssText=`text-align:left;margin:5px 10px`
+    if (msg.username === userName) {
+        message.innerText = `You: ${msg.message}`;
+        message.style.cssText += `
+            text-align:right;
+            margin:5px 10px;
+            width:200px;
+            max-width:50%;
+            padding:5px;
+            border-radius:5px;
+            background-color:#5cb85c;
+            color:#ffffff;
+            float:right;
+            clear:both;
+        `;
+    } else {
+        message.innerText = `${msg.username}: ${msg.message}`;
+        message.style.cssText += `
+            text-align:left;
+            margin:5px 10px;
+            width:200px;
+            max-width:50%;
+            padding:5px;
+            border-radius:5px;
+            background-color:#7d7d7d;
+            color:#ffffff;
+            clear:both;
+        `;
     }
     messages.appendChild(message)
+    messages.scrollTop = messages.scrollHeight;
 })
 
